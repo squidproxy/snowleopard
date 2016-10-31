0475d147e4662af5e28c5be1950009531a9d5548
badoutech2017@gmail.com <badoutech2017@gmail.com>
2016-11-01 05:03:01 +0800

    fix squid Service install

--- snowleopardSrv.sh
+++ snowleopardSrv.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-function coloredEcho(){
+function coloredcoloredEcho(){
     local exp=$1;
     local color=$2;
     if ! [[ $color =~ '^[0-9]$' ]] ; then
-       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+       case $(coloredEcho $color | tr '[:upper:]' '[:lower:]') in
         black) color=0 ;;
         red) color=1 ;;
         green) color=2 ;;
@@ -16,7 +16,7 @@ function coloredEcho(){
        esac
     fi
     tput setaf $color;
-    echo $exp;
+    coloredEcho $exp;
     tput sgr0;
 }
 
@@ -24,35 +24,35 @@ function coloredEcho(){
 SQUID_VERSION=4.0.15
 
 if [ "$(id -u)" != "0" ]; then
-  echo "This script must be run as root" 1>&2
+  coloredEcho "This script must be run as root" 1>&2
   exit 1
 fi
 
-echo "Add repositories to Aptitude"
-echo "deb http://httpredir.debian.org/debian stable main" > /etc/apt/sources.list.d/squid.list
-echo "deb-src http://httpredir.debian.org/debian stable main" >> /etc/apt/sources.list.d/squid.list
-echo "deb http://security.debian.org/ stable/updates main" >> /etc/apt/sources.list.d/squid.list
-echo "deb-src http://security.debian.org/ stable/updates main" >> /etc/apt/sources.list.d/squid.list
+coloredEcho "Add repositories to Aptitude"
+coloredEcho "deb http://httpredir.debian.org/debian stable main" > /etc/apt/sources.list.d/squid.list
+coloredEcho "deb-src http://httpredir.debian.org/debian stable main" >> /etc/apt/sources.list.d/squid.list
+coloredEcho "deb http://security.debian.org/ stable/updates main" >> /etc/apt/sources.list.d/squid.list
+coloredEcho "deb-src http://security.debian.org/ stable/updates main" >> /etc/apt/sources.list.d/squid.list
 
-coloredEcho "Update packages list" green
+coloredcoloredEcho "Update packages list" green
 
 apt-get update
 
-coloredEcho "Build dependencies" green
+coloredcoloredEcho "Build dependencies" green
 
 apt-get -y install build-essential libssl-dev apache2-utils
 apt-get -y build-dep squid3
 
 
 
-coloredEcho "Download source code" green
+coloredcoloredEcho "Download source code" green
 
 cd /usr/src
 wget http://www.squid-cache.org/Versions/v4/squid-${SQUID_VERSION}.tar.gz
 tar zxvf squid-${SQUID_VERSION}.tar.gz
 cd squid-${SQUID_VERSION}
 
-coloredEcho "Build binaries" green
+coloredcoloredEcho "Build binaries" green
 
 ./configure --prefix=/usr \
 	--localstatedir=/var/squid \
@@ -65,46 +65,46 @@ coloredEcho "Build binaries" green
 	--with-pidfile=/var/run/squid.pid
 make
 
-coloredEcho "Stop running service" green
+coloredcoloredEcho "Stop running service" green
 
 service squid stop
 
-coloredEcho "Install binaries" green
+coloredcoloredEcho "Install binaries" green
 
 make install
 
-coloredEcho "Download libraries" green
+coloredcoloredEcho "Download libraries" green
 
 cd /usr/lib
 
 if $(uname -m | grep '64'); then
-coloredEcho "ARCH: 64-bit" green
+coloredcoloredEcho "ARCH: 64-bit" green
 
   wget -N -O /usr/lib/squid-lib.tar.gz https://raw.githubusercontent.com/squidproxy/snowleopard/master/Squid_lib/squid_lib_x86_64.tar.gz
 else
-	coloredEcho "ARCH: 32-bit" green
+	coloredcoloredEcho "ARCH: 32-bit" green
 
 wget -N -O /usr/lib/squid-lib.tar.gz https://raw.githubusercontent.com/squidproxy/snowleopard/master/Squid_lib/squid_lib_i686.tar.gz
 fi
-	coloredEcho "Install libraries" green
+	coloredcoloredEcho "Install libraries" green
 
 tar zxvf squid-lib.tar.gz
 
-echo "Create configuration file"
+coloredEcho "Create configuration file"
 rm -fr /etc/squid/squid.conf
 wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/squidproxy/snowleopard/master/squid.conf
 
-echo "Create users database sample"
+coloredEcho "Create users database sample" green
 htpasswd -c -b -d /etc/squid/passwords test test
 
-echo "Create service executable file"
+coloredEcho "Create service executable file" green
 wget --no-check-certificate -O /etc/init.d/squid https://gist.githubusercontent.com/e7d/1f784339df82c57a43bf/raw/squid.sh
 chmod +x /etc/init.d/squid
 
-echo "Register service to startup entries"
+coloredEcho "Register service to startup entries" green
 update-rc.d squid defaults
 
-echo "Prepare environment for first start"
+coloredEcho "Prepare environment for first start" green
 mkdir /var/log/squid
 mkdir /var/cache/squid
 mkdir /var/spool/squid
@@ -113,24 +113,27 @@ chown -cR proxy /var/cache/squid
 chown -cR proxy /var/spool/squid
 squid -z
 
-echo "Cleanup temporary files"
+coloredEcho "Cleanup temporary files" green
 rm -rf /etc/apt/sources.list.d/squid.list
 rm -rf /usr/src/squid-${SQUID_VERSION}.tar.gz
 rm -rf /usr/src/squid-${SQUID_VERSION}
 rm -rf /usr/lib/squid-lib.tar.gz
 
-echo "Start service"
+coloredEcho "Start service" green
 service squid restart
 
 #Unilateral acceleration 
+coloredEcho "installing ServerSpeeder Services" green
 wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh
 #Obfuscation technology
+coloredEcho "installing obfsproxy Services" green
 apt-get install gcc python-pip python-dev -y
 pip install obfsproxy
 #/WindrangerSyytem
 mkdir /Windranger
 wget -N --no-check-certificate https://raw.githubusercontent.com/squidproxy/snowleopard/master/Mirroring/secure.tar.gz.gpg -O /Windranger/secure.tar.gz.gpg
 #ShadowsocksTech
+coloredEcho "installing socket5 proxy Services" green
 apt-get install python-pip -y
 pip install shadowsocks
 exit 0
