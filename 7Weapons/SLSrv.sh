d698eae5ecdd72dca4a323329d6693dc405bef33
badoutech2017@gmail.com <badoutech2017@gmail.com>
2017-02-14 16:41:33 +0800

    7 Weapons

---
+++ 7Weapons/SLSrv.sh
@@ -0,0 +1,128 @@
+#!/bin/sh
+
+function coloredEcho(){
+    local exp=$1;
+    local color=$2;
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+        black) color=0 ;;
+        red) color=1 ;;
+        green) color=2 ;;
+        yellow) color=3 ;;
+        blue) color=4 ;;
+        magenta) color=5 ;;
+        cyan) color=6 ;;
+        white|*) color=7 ;; # white or invalid color
+       esac
+    fi
+    tput setaf $color;
+    echo $exp;
+    tput sgr0;
+}
+
+SQUID_VERSION=3.5.24
+
+if [ "$(id -u)" != "0" ]; then
+  coloredEcho "This script must be run as root" 1>&2
+  exit 1
+fi
+
+
+apt-get update
+
+coloredEcho "Build dependencies" green
+
+apt-get -y install build-essential libssl-dev apache2-utils apache2
+apt-get -y build-dep squid3
+
+
+
+coloredEcho "Download source code" green
+
+cd /usr/src
+
+wget http://www.squid-cache.org/Versions/v3/3.5/squid-${SQUID_VERSION}.tar.gz
+tar zxvf squid-${SQUID_VERSION}.tar.gz
+cd squid-${SQUID_VERSION}
+
+coloredEcho "Build binaries" green
+
+./configure --prefix=/usr \
+	--localstatedir=/var/squid \
+	--libexecdir=${prefix}/lib/squid \
+	--srcdir=. \
+	--datadir=${prefix}/share/squid \
+	--sysconfdir=/etc/squid \
+	--with-default-user=proxy \
+	--with-logdir=/var/log/squid \
+	--with-pidfile=/var/run/squid.pid
+make
+
+coloredEcho "Stop running service" green
+
+service squid stop
+
+coloredEcho "Install binaries" green
+
+make install
+
+coloredEcho "Download libraries" green
+
+mkdir /usr/lib/squid 
+
+cd /usr/lib/squid
+
+
+if [ `getconf LONG_BIT` = "64" ]
+then
+    coloredEcho "ARCH: 64-bit" green
+
+  wget -N -O /usr/lib/squid/squid-lib.tar.gz https://goo.gl/9NBE8g
+else
+	coloredEcho "ARCH: 32-bit" green
+
+wget -N -O /usr/lib/squid/squid-lib.tar.gz https://goo.gl/tBwURG
+
+fi
+
+
+
+tar zxvf squid-lib.tar.gz
+
+coloredEcho "Create configuration file"
+rm -fr /etc/squid/squid.conf
+wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/squidproxy/snowleopard/master/squid.conf
+
+# Squid Safe authenticate
+# -c  Create a new password file
+# -b  Use the password from the command line rather than prompting for it.
+# -m Force MD5 encryption of the password (default)
+coloredEcho "Create users database sample" green
+htpasswd -c -b -m /etc/squid/passwords test test
+
+
+coloredEcho "Create service executable file" green
+wget --no-check-certificate -O /etc/init.d/squid https://gist.githubusercontent.com/e7d/1f784339df82c57a43bf/raw/squid.sh
+chmod +x /etc/init.d/squid
+
+coloredEcho "Register service to startup entries" green
+update-rc.d squid defaults
+
+coloredEcho "Prepare environment for first start" green
+mkdir /var/log/squid
+mkdir /var/cache/squid
+mkdir /var/spool/squid
+chown -cR proxy /var/log/squid
+chown -cR proxy /var/cache/squid
+chown -cR proxy /var/spool/squid
+squid -z
+
+coloredEcho "Cleanup temporary files" green
+rm -rf /etc/apt/sources.list.d/squid.list
+rm -rf /usr/src/squid-${SQUID_VERSION}.tar.gz
+rm -rf /usr/src/squid-${SQUID_VERSION}
+rm -rf /usr/lib/squid-lib.tar.gz
+
+coloredEcho "Start service" green
+service squid restart
+exit 0
